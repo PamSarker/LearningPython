e14038ab50a1bb27d37534983db3e2e53edee038
mailmesjp <6547494+mailmesjp@users.noreply.github.com>
2023-05-20 21:16:54 +0100

    Day1 Branch Update

--- Day1.py
+++ Day1.py
@@ -1,3 +1,9 @@
 # Day 1 File
 
 print("Hello World!")
+
+print("Welcome to the band name generator!")
+pet_name = input("what is the name of your first pet? ")
+city_name = input("what is the name of your city? ")
+
+print(f"Your band name is: {pet_name} {city_name}!")
